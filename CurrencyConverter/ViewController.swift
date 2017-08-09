a6b5905efb00dfae8a1cfb892c0a3af5dcdd73b8
Samuel Hardy <sam.s.hardy1@gmail.com>
2017-08-08 19:39:10 -0600

    Initial commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Samuel Hardy on 8/7/17.
+//  Copyright © 2017 Samuel Hardy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBAction func clearTextField(_ sender: UIButton) {
+        inputTextField.text = ""
+        poundLabel.text = "0.00"
+        yenLabel.text = "0.00"
+        euroLabel.text = "0.00"
+    }
+    
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var euroLabel: UILabel!
+    @IBOutlet weak var yenLabel: UILabel!
+    @IBOutlet weak var poundLabel: UILabel!
+    
+    let poundRate = 0.69
+    let yenRate = 113.94
+    let euroRate = 0.89
+    var dollarAmount = 0.0
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func convertCurrency(_ sender: UIButton) {
+        if let amount = Double(inputTextField.text!) {
+            dollarAmount = amount
+        }
+        poundLabel.text = "\(dollarAmount * poundRate)"
+        yenLabel.text = "\(dollarAmount * yenRate)"
+        euroLabel.text = "\(dollarAmount * euroRate)"
+        dollarAmount = 0.0
+    }
+
+    override func viewDidLoad() {
+        inputTextField.delegate = self
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
